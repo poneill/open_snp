f2f8a515ba9796be47d18d7d0a90eec29d2b593e
DMGin_000 <DMGinMD@nosuch.com>
2013-06-29 20:34:51 -0400

    Add elapsed time to output. Handle (I think) all the various file formats. Add counts of files by type (23andme vs. illumina, etc.) Eliminate attempt to sort by integer portion of RS number.  There are other RS formats with embedded text so this is not viable.

--- src/parseSNPs/parse_files.py
+++ src/parseSNPs/parse_files.py
@@ -1,23 +1,5 @@
 """
-This program is designed to parse SNP files from OpenSNP.  The file contents are tab-separated and look 
-generally like this (minus the column headings):
-
-   (1)       (2)     (3)    (4)
-rs3131972     1    742584    AG
-rs12184325    1    743968    CC
-rs3131969     1    744045    AG
-rs12562034    1    758311    GG
-rs12124819    1    766409    AA
-rs2518996     1    782397    GG
-rs12132517    1    788664    GG
-rs11240777    1    788822    GG
-
-The columns are:
-(1) RSID - The SNP identifier (an rsid or an internal id)
-(2) Chromosome #
-(3) Position on the chromosome on the reference human genome
-(3) The genotype call oriented with respect to the plus strand on the human reference 
-sequence using reference human assembly build 36.
+This program is designed to parse SNP files from OpenSNP.  
 
 Parameters are stored in an text file whose location is passed in as a parameter to the 
 program.  The file is tab-separated and contains identifier/value pairs.  Allowed 
@@ -41,11 +23,13 @@ SHOWLINEPROGRESS - If true, show the progress of files and the lines in them as 
 import sys
 import fnmatch
 import os
-import operator
+from datetime import datetime
 
 # import re
 
 true_values = ["TRUE", "T", "1", "YES", "Y"]
+start_time = datetime.now()
+
 def bool_param(param):
     if (param.upper() in true_values):
         value = True
@@ -90,6 +74,21 @@ def cleanup(params):
         params["SHOWLINEPROGRESS"] = False
     return
 
+# Remove leading and trailing double quotes
+def strip_quotes(val):
+    if val[0:1] == "\"":
+        val = val[1:]
+    if val[len(val)-1:] == "\"":
+        val = val[:len(val)-1]
+    return val
+# Increment a counter in a dictionary
+def increment(counter, value):
+    if (value in counter):
+        counter[value] += 1
+    else:
+        counter[value] = 1
+    return
+
 # Process the SNPs according to the params passed in.
 def process(params):
     cleanup(params)
@@ -104,10 +103,26 @@ def process(params):
     d = params["DIR"]
     directory = os.listdir(d)
     dir_count = 0
+    fileTypeCounts={}
+    
     # Count number of files to process for progress reporting
     for filename in directory:
         if (fnmatch.fnmatch(filename, params["FILE"])):
             dir_count += 1
+            if (fnmatch.fnmatch(filename, "*23andme.txt")):
+                increment(fileTypeCounts, "23andme")
+            elif (fnmatch.fnmatch(filename, "*illumina.txt")):
+                    increment(fileTypeCounts, "illumina")
+            elif (fnmatch.fnmatch(filename, "*IYG.txt")):
+                    increment(fileTypeCounts, "iyg")
+            elif (fnmatch.fnmatch(filename, "*decodeme.txt")):
+                    increment(fileTypeCounts, "decodeme")
+                    
+    # Summarize counts by file type
+    print
+    for count in fileTypeCounts.iteritems():
+        print count[0], ":", count[1]
+    print
             
     # Process each selected file.  Returns a dictionary structured as {rsid:{(chromosome, position):{genotype:count_of_occurences}}}
     for filename in directory:
@@ -115,19 +130,67 @@ def process(params):
             with open(os.path.join(d, filename)) as f:
                 files += 1
                 if params["SHOWFILEPROGRESS"] and not params["SHOWLINEPROGRESS"]:
-                    print "file: " + str(files) + "/" + str(dir_count) + "\r",
+                    elapsed = divmod((datetime.now() - start_time).total_seconds(), 60)
+                    print "files: %d/%d  elapsed: %d minutes, %d seconds\r" % (files, dir_count, elapsed[0], elapsed[1])
                 sys.stdout.flush()
-                lines = 0
+                lines_processed = 0
+                lines_read = 0
                 line = f.readline()
-                while (line != ""):
+                exit_file = False
+                while (line != "" and not exit_file):
+                    lines_read += 1
                     skip = False;
                     if (line[0:1] == "#"):
                         skip = True
                     else:
-                        data = line.strip().split("\t")
-                        if (len(data) != 4):
+                        # 23andme structure (tab separated):
+                        # rsid      chromosome    pos    genotype
+                        # rs4477212    1         72017    AA
+                        if (fnmatch.fnmatch(filename, "*23andme.txt")):
+                            data = line.strip().split("\t")
+                            if (len(data) != 4):
+                                skip = True
+                                
+                        # illumina can have two structures:
+                        elif (fnmatch.fnmatch(filename, "*illumina.txt")):
+                            if (line[0:1] == "\""):
+                                # RSID,CHROMOSOME,POSITION,RESULT
+                                # "rs3094315","1","742429","AA"
+                                data = map(strip_quotes, line.strip().split(",")) 
+                                if (len(data) != 4):
+                                    skip = True
+                            else:
+                                #rsid        chromosome   position    allele1    allele2
+                                #rs4477212        1        82154    T    T
+                                data = line.strip().split("\t")
+                                if (len(data) == 5):
+                                    data = data[0], data[1], data[2], data[3] + data[4]
+                                else:
+                                    skip = True
+                                    
+                        # IYG structure:
+                        # RSID,RESULT
+                        # rs2131925    TT
+                        elif (fnmatch.fnmatch(filename, "*IYG.txt")):
+                            data = strip_quotes(line.strip()).split("\t")
+                            if (len(data) == 2):
+                                data = (data[0], "0", "0", data[1])
+                            else:
+                                skip = True
+                                
+                        # decodeme structure:
+                        # Name,Variation,Chromosome,Position,Strand,YourCode
+                        # rs4345758,C/T,1,28663,+,TT
+                        elif (fnmatch.fnmatch(filename, "*decodeme.txt")):
+                            data = line.strip().split(",")
+                            if (len(data) == 6):
+                                data = (data[0], data[2], data[3], data[5])
+                            else:
+                                skip = True
+                        else: 
                             skip = True
-                        else:
+                            
+                        if (not skip):
                             try:
                                 chrom = int(data[1])
                             except ValueError:
@@ -136,19 +199,21 @@ def process(params):
                                 pos = int(data[2])
                             except ValueError:
                                 skip = True
-                    if (not skip):
+                    if (not skip and not len(data[0]) > 20):
                         rsid = data[0].upper()
                         genotype = data[3]
-    
-#                        if (rsid_re.match(rsid)                          
-                        if (fnmatch.fnmatch(rsid, params["RSID"]) 
-                            and chrom >= chrom_start and chrom <= chrom_end 
+                        
+                        # Optimization.  Relies on files being in chromosome # sequence
+                        if chrom > 0 and chrom > chrom_end:
+                            exit_file = True
+                            continue
+                        if (fnmatch.fnmatch(rsid, params["RSID"]) and chrom >= chrom_start 
                             and pos >= pos_start and pos <= pos_end):
-                            lines += 1
+                            lines_processed += 1
                             chrom_position = (chrom,pos)
                             if (rsid in rsids):
                                 chrom_positions = rsids[rsid]
-                                if [chrom_position in chrom_positions]:
+                                if chrom_position in chrom_positions:
                                     genotypes = chrom_positions[chrom_position]
                                     if (genotype in genotypes):
                                         genotypes[genotype] = genotypes[genotype] + 1
@@ -160,17 +225,24 @@ def process(params):
                                 rsids[rsid] = chrom_positions
                             else:
                                 rsids[rsid] = {(chrom_position):{genotype:1}}
-                            if params["SHOWLINEPROGRESS"]:
-                                print "file: " + str(files) + "/" + str(dir_count) \
-                                    + "  line: " + str(lines) + "\r", 
-                                sys.stdout.flush()
+                    if params["SHOWLINEPROGRESS"]:
+                        elapsed = divmod((datetime.now() - start_time).total_seconds(), 60)
+                        print "file: %d/%d  lines read: %d, processed %d  elapsed: %d minutes, %d seconds\r" % (files, dir_count, lines_read, lines_processed, elapsed[0], elapsed[1]) 
+                        sys.stdout.flush()
                     line = f.readline();
     return rsids
 
+"""
+# This doesn't work but I'm keeping it for now as an example of how to manipulate the keys in sorting a dictionary for output
 def rsid_key_seq( item ):
-    f=operator.itemgetter(0)
-    return int(f(item)[2:])
-        
+    # RS numbers can begin with "RS", "VG" or "I".  We want to sequence all the "I"s first, then the "R"s, then the "V"s.
+    if item[0:1] == "R":
+        return int(item[2:]) + 1000000000
+    elif item[0:1] == "V":
+        return int(item[2:]) + 5000000000
+    else:
+        return int(item[1:])
+"""        
 if __name__=="__main__":
     filename = sys.argv[1]
     params = {}
@@ -186,6 +258,16 @@ if __name__=="__main__":
 
 print "\n"
 
-for entry in sorted(results.items(), key=lambda t: int(t[0][2:])):
-    print entry
+if (len(results) > 1):
+#    for entry in sorted(results.items(), key=lambda t: rsid_key_seq(t[0])):
+    for entry in sorted(results.items()):
+        print entry[0], ":", entry[1], "           " # Add whitespace to ensure there are no leftover chars when we overprint the prior line.
+else:
+    print "Nothing matched selections"
+
+elapsed = divmod((datetime.now() - start_time).total_seconds(), 60)
+print
+print "Elapsed: %d minutes, %d seconds" % (elapsed[0], elapsed[1])
+
+
             
