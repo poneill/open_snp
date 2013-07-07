e52b59a14fd3c0914ccbe6e147d951ed8bd1d4ba
DMGin_000 <DMGinMD@nosuch.com>
2013-07-07 18:50:15 -0400

    Move documentation of the options file into the file.
    Most notably, allow the options file (and the processing) to specify
    multiple groups of files, each containing lists of file selectors.  One
    we start applying the phenotypes, we will want to compare specific users
    - tongue rollers vs. non-tongue-rollers, for example.  So the file
    format now allows us to specify a first group containing only those with
    the "tongue roller" phenotype and a second group containing only those
    who are classed as non-tongue rollers.
    
    Added optional output of the filenames of selected files and files with
    no data to process that meets the selection parameters.  Modified the
    option to print line number detail so that we can limit the output to
    every nth line.
    
    Add constants for the selection parameter names.
    
    The internal parsed structure is now
    {rsids:{phonotype_label:{(chromosome_number,position):{genotype:count}}}}

--- src/parseSNPs/parse_files.py
+++ src/parseSNPs/parse_files.py
@@ -2,33 +2,30 @@
 This program is designed to parse SNP files from OpenSNP.  
 
 Parameters are stored in an text file whose location is passed in as a parameter to the 
-program.  The file is tab-separated and contains identifier/value pairs.  Allowed 
-identifiers include:
-DIR - The directory the SNP files are found in.  If "." or unspecified, the current directory
-    is used.
-FILE - The name of the SNP file to process.  If "*" or unspecified, all files are processed.
-RSID - The RSID to search for int The file.  If unspecified or "*", all RSIDs are processed.
-CHROMSTART - The starting chromosome to process.  If unspecified, all up to the CHROMEND# 
-    are processed
-CHROMEND - The last chromosome to process.  If unspecified, all starting with CHROMSTART# 
-    are processed
-POSSTART - The starting position on the chromosome to process.  If unspecified, all positions 
-    up to the END# are processed
-POSEND - The last position on the chromosome to process.  If unspecified, all positions starting 
-    with START# are processed
-SHOWFILEPROGRESS - If true, show the progress of files as we process each
-SHOWLINEPROGRESS - If true, show the progress of files and the lines in them as we process each
+program.  The file is tab-separated and contains identifier/value pairs.  See parsefiles.txt 
+for an example.
 """
 
 import sys
 import fnmatch
 import os
+import parser
 from datetime import datetime
 
 # import re
 
 true_values = ["TRUE", "T", "1", "YES", "Y"]
 start_time = datetime.now()
+DIR = "DIR"
+FILES = "FILES"
+RSID  = "RSID"
+CHROMSTART = "CHROMSTART"
+CHROMEND = "CHROMEND"
+POSSTART = "POSSTART"
+POSEND = "POSEND"
+SHOWFILEPROGRESS = "SHOWFILEPROGRESS"
+SHOWPROGRESSLINES = "SHOWPROGRESS#LINES"
+SHOWSELECTEDFILES = "SHOWSELECTEDFILES"
 
 def bool_param(param):
     if (param.upper() in true_values):
@@ -40,38 +37,42 @@ def bool_param(param):
     
 # Process the SNPs according to the params passed in.
 def cleanup(params):
-    if ("DIR" not in params):
-        params["DIR"] = "."
-    if ("FILE" not in params):
-        params["FILE"] = "*"
-    if ("RSID" in params):
-        params["RSID"] = params["RSID"].upper()
+    if (DIR not in params):
+        params[DIR] = "."
+    if (FILES not in params):
+        params[FILES] = {{0,"Default"}, ["*"]}
+    if (RSID in params):
+        params[RSID] = params[RSID].upper()
     else:
-        params["RSID"] = "*"        
-    if ("CHROMSTART" in params):
-        params["CHROMSTART"] = int(params["CHROMSTART"])
+        params[RSID] = "*"        
+    if (CHROMSTART in params):
+        params[CHROMSTART] = int(params[CHROMSTART])
     else:
-        params["CHROMSTART"] = 0
-    if ("CHROMEND" in params):
-        params["CHROMEND"] = int(params["CHROMEND"])
+        params[CHROMSTART] = 0
+    if (CHROMEND in params):
+        params[CHROMEND] = int(params[CHROMEND])
     else:
-        params["CHROMEND"] = 23
-    if ("POSSTART" in params):
-        params["POSSTART"] = int(params["POSSTART"])
+        params[CHROMEND] = 23
+    if (POSSTART in params):
+        params[POSSTART] = int(params[POSSTART])
     else:
-        params["POSSTART"] = 0
-    if ("POSEND" in params):
-        params["POSEND"] = int(params["POSEND"])
+        params[POSSTART] = 0
+    if (POSEND in params):
+        params[POSEND] = int(params[POSEND])
     else:
-        params["POSEND"] = sys.maxint
-    if ("SHOWFILEPROGRESS" in params):
-        params["SHOWFILEPROGRESS"] = bool_param(params["SHOWFILEPROGRESS"])
+        params[POSEND] = sys.maxint
+    if (SHOWFILEPROGRESS in params):
+        params[SHOWFILEPROGRESS] = bool_param(params[SHOWFILEPROGRESS])
     else:
-        params["SHOWFILEPROGRESS"] = False
-    if ("SHOWLINEPROGRESS" in params):
-        params["SHOWLINEPROGRESS"] = bool_param(params["SHOWLINEPROGRESS"])
+        params[SHOWFILEPROGRESS] = False
+    if (SHOWPROGRESSLINES in params):
+        params[SHOWPROGRESSLINES] = int(params[SHOWPROGRESSLINES])
     else:
-        params["SHOWLINEPROGRESS"] = False
+        params[SHOWPROGRESSLINES] = 0
+    if (SHOWSELECTEDFILES in params):
+        params[SHOWSELECTEDFILES] = bool_param(params[SHOWSELECTEDFILES])
+    else:
+        params[SHOWSELECTEDFILES] = false
     return
 
 # Remove leading and trailing double quotes
@@ -89,6 +90,21 @@ def increment(counter, value):
         counter[value] = 1
     return
 
+# If the file matches one of the file patterns in the parm, return the label to use
+def file_name_label(filename, parm):
+    label = ""
+    for entry in sorted(parm.items()):
+        for file_match in entry[1]:
+            if fnmatch.fnmatch(filename, file_match):
+                label = entry[0][1]
+                break
+        if label != "":
+            break
+    return label
+
+def bypass(filename):
+    return "-exome-" in filename
+
 # Process the SNPs according to the params passed in.
 def process(params):
     cleanup(params)
@@ -96,11 +112,11 @@ def process(params):
     files = 0
     print "Processing files"
 #    rsid_re = re.compile(params["RSID"])
-    chrom_start = params["CHROMSTART"]
-    chrom_end = params["CHROMEND"]
-    pos_start = params["POSSTART"]
-    pos_end = params["POSEND"]
-    d = params["DIR"]
+    chrom_start = params[CHROMSTART]
+    chrom_end = params[CHROMEND]
+    pos_start = params[POSSTART]
+    pos_end = params[POSEND]
+    d = params[DIR]
     directory = os.listdir(d)
     dir_count = 0
     fileTypeCounts={}
@@ -108,7 +124,11 @@ def process(params):
     
     # Count number of files to process for progress reporting
     for filename in directory:
-        if (fnmatch.fnmatch(filename, params["FILE"])):
+        if (bypass(filename)):
+            continue
+        if (file_name_label(filename, params[FILES]) != ""):
+            if params[SHOWSELECTEDFILES]: 
+                print "Selected:", filename
             dir_count += 1
             if (fnmatch.fnmatch(filename, "*23andme.txt")):
                 increment(fileTypeCounts, "23andme")
@@ -127,10 +147,14 @@ def process(params):
             
     # Process each selected file.  Returns a dictionary structured as {rsid:{(chromosome, position):{genotype:count_of_occurences}}}
     for filename in directory:
-        if (fnmatch.fnmatch(filename, params["FILE"])):
+        label = file_name_label(filename, params[FILES])
+        if (label != ""):
+            if (bypass(filename)):
+                skipped_files.append(filename)
+                continue
             with open(os.path.join(d, filename)) as f:
                 files += 1
-                if params["SHOWFILEPROGRESS"] and not params["SHOWLINEPROGRESS"]:
+                if params[SHOWFILEPROGRESS] and params[SHOWPROGRESSLINES] <= 0:
                     elapsed = divmod((datetime.now() - start_time).total_seconds(), 60)
                     print "files: %d/%d  elapsed: %d minutes, %d seconds\r" % (files, dir_count, elapsed[0], elapsed[1])
                 sys.stdout.flush()
@@ -202,42 +226,47 @@ def process(params):
                                 pos = int(data[2])
                             except ValueError:
                                 skip = True
-                    if len(data[0]) > 20:
-                        skipped_files.append(filename)
-                        exit_file = True
-                    elif not skip:
+                        if len(data[0]) > 20:
+                            skipped_files.append(filename)
+                            exit_file = True
+                    if not exit_file and not skip:
                         rsid = data[0].upper()
                         genotype = data[3]
                         
                         # Optimization.  Relies on files being in chromosome # sequence
-                        if (rsid == "RS10403190"):
-                            i = 1
                         if chrom_end > 0 and chrom > chrom_end:
                             exit_file = True
                             continue
-                        if (fnmatch.fnmatch(rsid, params["RSID"]) and chrom >= chrom_start 
+                        if (fnmatch.fnmatch(rsid, params[RSID]) and chrom >= chrom_start 
                             and pos >= pos_start and pos <= pos_end):
                             lines_processed += 1
                             chrom_position = (chrom,pos)
                             if (rsid in rsids):
-                                chrom_positions = rsids[rsid]
-                                if chrom_position in chrom_positions:
-                                    genotypes = chrom_positions[chrom_position]
-                                    if (genotype in genotypes):
-                                        genotypes[genotype] = genotypes[genotype] + 1
+                                labels = rsids[rsid]
+                                if (label in labels):
+                                    chrom_positions = labels[label]
+                                    if chrom_position in chrom_positions:
+                                        genotypes = chrom_positions[chrom_position]
+                                        if (genotype in genotypes):
+                                            genotypes[genotype] = genotypes[genotype] + 1
+                                        else:
+                                            genotypes[genotype] = 1
+                                        chrom_positions[chrom_position] = genotypes
                                     else:
-                                        genotypes[genotype] = 1
-                                    chrom_positions[chrom_position] = genotypes
+                                        chrom_positions[chrom_position] = {genotype:1}
+                                    labels[label] = chrom_positions
                                 else:
-                                    chrom_positions[chrom_position] = {genotype:1}
-                                rsids[rsid] = chrom_positions
+                                    labels[label] = {(chrom_position):{genotype:1}}
+                                rsids[rsid] = labels
                             else:
-                                rsids[rsid] = {(chrom_position):{genotype:1}}
-                    if params["SHOWLINEPROGRESS"]:
+                                rsids[rsid] = {label:{(chrom_position):{genotype:1}}}
+                    if params[SHOWPROGRESSLINES] > 0 and lines_read % params[SHOWPROGRESSLINES] == 0:
                         elapsed = divmod((datetime.now() - start_time).total_seconds(), 60)
-                        print "file: %d/%d  lines read: %d, processed %d  elapsed: %d minutes, %d seconds\r" % (files, dir_count, lines_read, lines_processed, elapsed[0], elapsed[1]) 
+                        print "file: {:,d}/{:,d}  lines read: {:,d}  processed {:,d}  elapsed: {:,d} minutes, {:,d} seconds\r".format(files, dir_count, lines_read, lines_processed, int(elapsed[0]), int(elapsed[1])) 
                         sys.stdout.flush()
-                    line = f.readline();
+                    line = f.readline()
+                if (params[SHOWSELECTEDFILES] and lines_processed == 0):
+                    print "    No lines to process in ", filename
     if(len(skipped_files) > 0):
         print 
         print "Skipped Files"
@@ -259,15 +288,35 @@ def rsid_key_seq( item ):
 if __name__=="__main__":
     filename = sys.argv[1]
     params = {}
-    with open(filename) as f:
-        line = f.readline()
-        while (line != ""):
+    f=file(filename,"r")
+    for line in f:
             if line[0:1] != "#":
                 values = line.strip().split("\t")
                 if (len(values) > 1 and values[0].strip()):
-                    params[values[0].strip().upper()]=values[1].strip()
-            line = f.readline()
-
+                    name = values[0].strip()
+                    val = values[1].strip()
+                    if (name[0:len(FILES)] == FILES):
+                        # If this is a files param, assemble all the file values as a 
+                        # dictionary in the form {(priority,label): [fileName, fileName, fileName]}
+                        # so if multiple lines have the same (priority,label) combination, they
+                        # will be consolidated
+                        priority_label = (sys.maxint,"No Label") # default
+                        priority_label_vals = name.split(":")
+                        priority_label_len = len(priority_label_vals)
+                        if (priority_label_len > 2):
+                            priority_label = (int(priority_label_vals[2]), priority_label_vals[1])
+                        priority_labels = {} # default
+                        if FILES in params:
+                            priority_labels = params[FILES]  # Get previously parsed files
+                        files_list = [] # default
+                        if priority_label in priority_labels:
+                            files_list = priority_labels[priority_label]                            
+                        files_list.extend([x.strip() for x in val.split(",")])
+                        priority_labels[priority_label] = files_list
+                        name = FILES
+                        val = priority_labels
+                    params[name.upper()]=val
+    f.close()
     results = process(params)
 
 print "\n"
